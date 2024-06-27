922291a15f58a088e06832a37d37a76a81c6d591
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-06-27 11:06:47 +0200

    Initial Commit

---
+++ SideMenu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideMenu
+//
+//  Created by Manyuchi, Carrington C on 2024/06/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
